65598112d30536e71d59114419ea590ca3c046d3
Ajo Paul <apaul@echoice.com.au>
2017-08-22 16:47:11 +1000

    added maven dep

---
+++ src/main/java/web/WSMain.java
@@ -0,0 +1,22 @@
+package web;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by ajopaul on 22/8/17.
+ */
+ @Path("/hello")
+public class WSMain {
+    @GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Jersey say : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+}
